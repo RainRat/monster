de3a08583197d9a724d0e87b970f2fd6b8a7e2d6
Bryce Wilson <brycemwilson@gmail.com>
2025-12-28 09:05:11 -0500

    Refactor bin/ directory

--- src/make-release.sh
+++ src/make-release.sh
@@ -1,21 +1,32 @@
+###############################################################################
+# Vic-20
+###############################################################################
 # make the installer
 make clean; make installer.prg
-cp installer.prg ../bin/
+cp installer.prg ../bin/vic20
 
 # make the NTSC cart binary
-make clean; make cart REGION=NTSC
-cp monster.bin ../bin/monster-ntsc.bin
+make clean;TARGET=vic20 make cart REGION=NTSC
+cp monster.bin ../bin/vic20/monster-ntsc.bin
 
 # make the PAL cart binary
-make clean; make cart REGION=PAL
-cp monster.bin ../bin/monster-pal.bin
+make clean;TARGET=vic20 make cart REGION=PAL
+cp monster.bin ../bin/vic20/monster-pal.bin
 
 # make the NTSC disk image
-make clean; make disk REGION=NSTC
+make clean;TARGET=vic20 make disk REGION=NSTC
 c1541 -format monster,1 d81 monster.d81 -attach monster.d81 -write boot.prg -write masm.prg
-cp monster.d81 ../bin/monster-ntsc.d81
+cp monster.d81 ../bin/vic20/monster-ntsc.d81
 
 # make the PAL disk image
-make clean; make disk REGION=PAL
+make clean; TARGET=vic20 make disk REGION=PAL
 c1541 -format monster,1 d81 monster.d81 -attach monster.d81 -write boot.prg -write masm.prg
-cp monster.d81 ../bin/monster-pal.d81
+cp monster.d81 ../bin/vic20/monster-pal.d81
+
+###############################################################################
+# C-64
+###############################################################################
+# make the universal disk image
+make clean; TARGET=c64 make disk REGION=NSTC
+c1541 -format monster,1 d81 monster.d81 -attach monster.d81 -write boot.prg -write masm.prg
+cp monster.d81 ../bin/c64/monster.d81
